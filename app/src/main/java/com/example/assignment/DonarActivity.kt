b0fba8dd57ddae7b1a1a4247a3c565486755179b
xuezhi0401 <tay.tt19@gmail.com>
2023-09-19 14:59:27 +0800

    19/9/2023 3pm

--- app/src/main/java/com/example/assignment/DonarActivity.kt
+++ app/src/main/java/com/example/assignment/DonarActivity.kt
@@ -3,6 +3,11 @@ package com.example.assignment
 import android.content.Intent
 import android.os.Bundle
 import android.provider.ContactsContract.ProfileSyncState
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
@@ -17,16 +22,34 @@ import androidx.fragment.app.Fragment
 import com.example.assignment.databinding.ActivityDonarBinding
 import com.example.assignment.ui.theme.AssignmentTheme
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class DonarActivity : AppCompatActivity() {
 
+    private var db = Firebase.firestore
+    private lateinit var binding: ActivityDonarBinding
+
+    private lateinit var editFoodName: EditText
+    private lateinit var editDes: EditText
+    private lateinit var food: RadioButton
+    private lateinit var drink: RadioButton
+    private lateinit var submitBtn: Button
+    private lateinit var cancelBtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_donar)
+        binding = ActivityDonarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.buttonNavigationView)
 
-// Set up a listener for item clicks
+        editFoodName = findViewById(R.id.editFoodName)
+        editDes = findViewById(R.id.editDes)
+        food = findViewById(R.id.food)
+        drink = findViewById(R.id.drink)
+
+        // Set up a listener for item clicks
         bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.home -> {
@@ -49,5 +72,44 @@ class DonarActivity : AppCompatActivity() {
             }
         }
 
+        binding.submitBtn.setOnClickListener {
+            val editFoodName = editFoodName.text.toString()
+            val editDes = editDes.text.toString()
+
+            val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
+
+            val selectedValue = when (radioGroup.checkedRadioButtonId) {
+                R.id.food -> "FOOD"
+                R.id.drink -> "DRINK"
+                else -> "No selection"
+            }
+
+            val currentUser = FirebaseAuth.getInstance().currentUser
+            if (currentUser != null) {
+                val userId = currentUser.uid
+
+                val foodMap = hashMapOf(
+                    "foodName" to editFoodName,
+                    "foodDes" to editDes,
+                    "foodOrDrink" to selectedValue // Store the selected value, not the RadioGroup
+                )
+
+                // Reference a new document with a generated ID
+                db.collection("food").add(foodMap)
+                    .addOnSuccessListener {
+                        Toast.makeText(this, "Successfully Create Food!", Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnFailureListener{
+                        Toast.makeText(this, "Failed To Create Food!", Toast.LENGTH_SHORT).show()
+                    }
+            } else {
+                // Handle the case where the user is not signed in
+                Toast.makeText(this, "User not signed in", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.cancelBtn.setOnClickListener {
+            finish()
+        }
     }
 }
